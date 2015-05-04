a4b3087122b19e844148b7d31ad6e62f50bb6e68
smallproject <j.a.villas@live.com.ph>
2015-05-04 16:21:50 +0200

    Start Empty

---
+++ source/Quick_ImageResize/Quick_ImageResize/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+
+namespace Quick_ImageResize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        private string Dimensions(string ImageUse)
+        {
+            string dimension = "";
+            switch (ImageUse)
+            {
+                case "facebook":
+                    dimension = "851,315";
+                    break;
+                default:
+                    break;
+            }
+
+            return dimension;
+        }
+
+        private string GetFiles()
+        {
+            FileStream stream = new FileStream();
+        }
+    }
+}
